f1230f3673aacceac4dfd221e4ab5bdfaddc7e68
faradawn <faradawny@gmail.com>
2025-09-29 14:42:01 +0000

    add small model result

--- cs336_systems/benchmarking_script.py
+++ cs336_systems/benchmarking_script.py
@@ -3,13 +3,20 @@ from cs336_basics.optimizer import AdamW
 from cs336_basics.nn_utils import cross_entropy
 import torch
 import argparse
+import timeit
+import numpy as np
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Faradawn Parser')
-    parser.add_argument('--num_layers', type=int)
-    parser.add_argument('--num_epochs', type=int, default=1)
     parser.add_argument('--num_warmup', type=int, default=5)
+    parser.add_argument('--num_epochs', type=int, default=10)
     parser.add_argument('--max_seq_len', type=int, default=256)
+
+    parser.add_argument('--d_model', type=int, default=768)
+    parser.add_argument('--d_ff', type=int, default=3072)
+    parser.add_argument('--num_layers', type=int, default=12)
+    parser.add_argument('--num_heads', type=int, default=12)
+
     args = parser.parse_args()
     print(args)
 
@@ -19,21 +26,38 @@ if __name__ == "__main__":
     batch_size = 4
     
 
-    model = BasicsTransformerLM(vocab_size=vocab_size, context_length=1024, d_model=256, num_layers=args.num_layers, num_heads=4, d_ff=256, rope_theta=1).to(device)
+    model = BasicsTransformerLM(vocab_size=vocab_size, context_length=args.max_seq_len, d_model=args.d_model, num_layers=args.num_layers, num_heads=args.num_heads, d_ff=args.d_ff, rope_theta=1).to(device)
 
     optimizer = AdamW(params=model.parameters())
 
     x = torch.randint(low=0, high=vocab_size, size=(batch_size, args.max_seq_len), dtype=torch.long, device=device)
     targets = torch.randint(low=0, high=vocab_size, size=(batch_size, args.max_seq_len), dtype=torch.long, device=device)
     
+    forward_times = []
+    backward_times = []
     for i in range(args.num_epochs):
+        t0 = timeit.default_timer()
         logits = model(x)
-        print("logits size", logits.size())
+        t1 = timeit.default_timer()
+        forward_times.append(t1-t0)
 
-        loss = cross_entropy(logits.view(-1, vocab_size), targets)
+        # TODO: how to zero grad 
+        optimizer.zero_grad()
+        loss = cross_entropy(logits.view(-1, vocab_size), targets.view(-1)) # inputs: [B*T, vocab_size]. targets: [B*T, ]
+        start = timeit.default_timer()
         loss.backward()
+        end = timeit.default_timer()
+        backward_times.append(end-start)
         optimizer.step()
-        print("Done training step")
+        
+        # print(f"i {i}, forward time {t1-t0}, backward time {end-start}")
+
+    result = {}
+    result["forward_time"] = sum(forward_times) / len(forward_times)
+    result["backward_time"] = sum(backward_times) / len(backward_times)
+    result["forward_var"] = np.var(forward_times, ddof=1)
+    result["backward_var"] = np.var(backward_times, ddof=1)
+    print(result)
 
     
 
