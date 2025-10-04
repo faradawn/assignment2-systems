1e84314e3e5274f7811ea9fd46f3d374287815ab
Faradawn Yang <faradawny@gmail.com>
2025-10-04 22:50:14 +0000

    add flashattention result
    Signed-off-by: Faradawn Yang <73060648+faradawn@users.noreply.github.com>

--- cs336_systems/benchmarking_script.py
+++ cs336_systems/benchmarking_script.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     parser.add_argument('--num_warmup', type=int, default=5)
     parser.add_argument('--num_epochs', type=int, default=10)
     parser.add_argument('--max_seq_len', type=int, default=256)
+    parser.add_argument('--batch_size', type=int, default=4)
 
     parser.add_argument('--d_model', type=int, default=768)
     parser.add_argument('--d_ff', type=int, default=3072)
@@ -26,15 +27,13 @@ if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print("device", device)
     vocab_size = 10000  # set this to your vocab size
-    batch_size = 4
-    
 
     model = BasicsTransformerLM(vocab_size=vocab_size, context_length=args.max_seq_len, d_model=args.d_model, num_layers=args.num_layers, num_heads=args.num_heads, d_ff=args.d_ff, rope_theta=1).to(device).to(torch.bfloat16)
 
     optimizer = AdamW(params=model.parameters())
 
-    x = torch.randint(low=0, high=vocab_size, size=(batch_size, args.max_seq_len), dtype=torch.long, device=device)
-    targets = torch.randint(low=0, high=vocab_size, size=(batch_size, args.max_seq_len), dtype=torch.long, device=device)
+    x = torch.randint(low=0, high=vocab_size, size=(args.batch_size, args.max_seq_len), dtype=torch.long, device=device)
+    targets = torch.randint(low=0, high=vocab_size, size=(args.batch_size, args.max_seq_len), dtype=torch.long, device=device)
 
     for i in range(args.num_warmup):
         logits = model(x)
@@ -65,8 +64,8 @@ if __name__ == "__main__":
     result = {}
     result["forward_time"] = sum(forward_times) / len(forward_times)
     result["backward_time"] = sum(backward_times) / len(backward_times)
-    result["forward_var"] = np.var(forward_times, ddof=1)
-    result["backward_var"] = np.var(backward_times, ddof=1)
+    # result["forward_var"] = np.var(forward_times, ddof=1)
+    # result["backward_var"] = np.var(backward_times, ddof=1)
     print(result)
 
     
