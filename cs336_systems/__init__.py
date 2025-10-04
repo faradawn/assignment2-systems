3e40ca97124498b1e1c2c510e2ea7e12240380d6
Faradawn Yang <faradawny@gmail.com>
2025-10-04 21:38:08 +0000

    add pure pip packaging command
    Signed-off-by: Faradawn Yang <faradawny@gmail.com>

--- cs336_systems/__init__.py
+++ cs336_systems/__init__.py
@@ -1,3 +1,7 @@
-import importlib.metadata
+# cs336_systems/__init__.py
+from importlib.metadata import version, PackageNotFoundError
 
-__version__ = importlib.metadata.version("cs336-systems")
+try:
+    __version__ = version("cs336-systems")
+except PackageNotFoundError:
+    __version__ = "0.0.0"  # fallback when running from source
